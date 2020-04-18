d0463ca5f6ea3db4f471ef5b6bc68281707a24b7
hmbashar <hmbashar@gmail.com>
2020-04-18 13:47:52 +0600

    init

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package CB_POPUP_Subscriber
+ * @version 1.0
+ */
+/*
+Plugin Name: CB Popup Subscriber
+Plugin URI: http://wordpress.org/plugins/cb-popup-subscriber
+Description: popup Email Subscription plugin
+Author: Md Abul Bashar
+Version: 1.0
+Author URI: https://facebook.com/hmbashar
+*/
